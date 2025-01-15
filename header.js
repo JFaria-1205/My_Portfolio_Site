a7794760f76f11fed0e87fc2d9e411cfeaf2496c
Jason Faria <jasonfaria01@gmail.com>
2025-01-15 14:56:45 -0500

    Initial Files
    Initial site files and setup

---
+++ header.js
@@ -0,0 +1,12 @@
+// Header HTML 
+const siteHeader = `
+    <header class="sticky-header">
+        <nav>
+            <ul>
+                <li><a href="index.html>Home</a></li>
+            </ul>
+        </nav>
+    </header>
+`;
+
+document.body.insertAdjacentHTML('afterbegin', siteHeader);
