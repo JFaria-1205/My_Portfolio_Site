978e6ba61ab191cd822569ab61bfa22c36627a52
Jason Faria <jasonfaria01@gmail.com>
2025-01-21 11:42:45 -0500

    update header
    header updates, added id's to nav links to show which is current page using js, updated header for all pages, more work on header styling.

--- assets/js/header.js
+++ assets/js/header.js
@@ -1,20 +1,22 @@
-// Header HTML 
-const siteHeader = `
-    <header class="sticky top-0 bg-gray-800 p-4 shadow-md grid grid-cols-2 items-center">
-        <div class="text-white font-thin text-2xl z-10">
-            <nav>
-                <ul class="flex justify-start space-x-4">
-                    <li><a href="index.html" class="hover:text-yellow-200">Home</a></li>
-                    <li><a href="projects.html" class="hover:text-yellow-300">Projects</a></li>
-                    <li><a href="about.html" class="hover:text-yellow-400">About</a></li>
-                    <li><a href="contact.html" class="hover:text-yellow-500">Contact</a></li>
-                </ul>
-            </nav>
-        </div>
-        <div class="text-white font-thin text-2xl text-right z-0">
-            <a href="index.html" class="hover:text-yellow-200">Jason Faria | Portfolio</a>
-        </div>        
-    </header>
-`;
+// Header HTML
+if (typeof siteHeader ==="undefined"){
+    const siteHeader = `
+        <header class="sticky top-0 grid grid-cols-2 items-center">
+            <div class="font-thin text-xl z-10">
+                <nav>
+                    <ul class="flex justify-start space-x-4">
+                        <li><a id="nav_home_link" href="index.html" class="hover:text-yellow-200">Home</a></li>
+                        <li><a id="nav_projects_link" href="projects.html" class="hover:text-yellow-300">Projects</a></li>
+                        <li><a id="nav_about_link" href="about.html" class="hover:text-yellow-400">About</a></li>
+                        <li><a id="nav_contact_link" href="contact.html" class="hover:text-yellow-500">Contact</a></li>
+                    </ul>
+                </nav>
+            </div>
+            <div class="font-thin text-xl text-right z-0">
+                <a href="index.html" class="hover:text-yellow-200">Jason Faria | Portfolio</a>
+            </div>
+        </header>
+    `;
 
-document.body.insertAdjacentHTML('afterbegin', siteHeader);
+    document.body.insertAdjacentHTML('afterbegin', siteHeader);
+}
--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -6,3 +6,37 @@ window.addEventListener('scroll', function () {
     // Apply the offset
     document.body.style.backgroundPosition = `center -${backgroundOffset}px`;
 });
+
+document.addEventListener("DOMContentLoaded", function () {
+    updateNavCurrentPage();
+});
+
+function updateNavCurrentPage() {
+    let homeLink = document.getElementById("nav_home_link");
+    let projectsLink = document.getElementById("nav_projects_link");
+    let aboutLink = document.getElementById("nav_about_link");
+    let contactLink = document.getElementById("nav_contact_link");
+
+    switch (window.location.pathname) {
+        case "/index.html":
+            homeLink.style.textDecoration = "underline";
+            break;
+        
+        case "/projects.html":
+            projectsLink.style.textDecoration = "underline";
+            break;
+
+        case "/about.html":
+            aboutLink.style.textDecoration = "underline";
+            break;
+
+        case "/contact.html":
+            contactLink.style.textDecoration = "underline";
+            break;   
+        
+        default:
+            homeLink.style.textDecoration = "underline";
+            console.error("updateNavCurrentPage(scripts.js): no html found; using default");
+            break;
+    }
+}
