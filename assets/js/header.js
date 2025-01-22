346f78304a7e3aa9095c606d27c36fe5bb19075e
Jason Faria <jasonfaria01@gmail.com>
2025-01-22 15:22:00 -0500

    Header updates for resizing
    Added responsive resizing for site window; added new menu with menu button for smaller screens (phones, smaller windows, etc.); fully functioning mini menu; imported image for menu (custom made in krita as well); minor tweaks to code / clean ups

--- assets/js/header.js
+++ assets/js/header.js
@@ -2,19 +2,30 @@
 if (typeof siteHeader ==="undefined"){
     const siteHeader = 
     `
-        <header class="sticky grid grid-cols-3 items-center">
-            <a id="nav-extra" class="font-thin text-lg z-0" href="test.html">Jason Faria</a>
+        <header id="head" class="sticky grid grid-cols-3 items-center">
+            <a id="nav-extra" class="hide-on-800 font-thin text-lg z-0" href="test.html">Jason Faria</a>
             
-            <nav class="font-thin text-xl z-10">
+            <nav class="hide-on-530 font-thin text-xl z-10">
                 <ul class="flex justify-center space-x-20">
-                    <li><a id="nav_home_link" href="index.html" class="link-hover">Home</a></li>
-                    <li><a id="nav_projects_link" href="projects.html" class="link-hover">Projects</a></li>
-                    <li><a id="nav_about_link" href="about.html" class="link-hover">About</a></li>
-                    <li><a id="nav_contact_link" href="contact.html" class="link-hover">Contact</a></li>
+                    <li><a href="index.html" class="nav_home_link link-hover">Home</a></li>
+                    <li><a href="projects.html" class="nav_projects_link link-hover">Projects</a></li>
+                    <li><a href="about.html" class="nav_about_link link-hover">About</a></li>
+                    <li><a href="contact.html" class="nav_contact_link link-hover">Contact</a></li>
                 </ul>
             </nav>
             
-            <a id="nav-extra" class="font-thin text-lg text-right z-0" href="index.html">Portfolio</a>
+            <a id="nav-extra" class="hide-on-800 font-thin text-lg text-right z-0" href="index.html">Portfolio</a>
+
+            <ul id="head-menu" class="show-on-530 font-thin text-xl z-10 items-center">
+                <li id="dropdown-button" class="link-hover-menu">
+                    <img id="nav-menu-pic" src="assets/images/nav-menu-button.png">
+                </li>
+                <ul id="dropdown-content" style="display: none;">
+                    <li><a href="index.html" class="nav_home_link link-hover-menu">Home</a></li>
+                    <li><a href="projects.html" class="nav_projects_link link-hover-menu">Projects</a></li>
+                    <li><a href="about.html" class="nav_about_link link-hover-menu">About</a></li>
+                    <li><a href="contact.html" class="nav_contact_link link-hover-menu">Contact</a></li>
+            </ul>    
         </header>
     `;
 
--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,3 +1,8 @@
+//#region Global Variables
+
+//#endregion
+
+//#region Event-Listeners
 window.addEventListener('scroll', function () {
     const scrollY = window.scrollY; // Get the vertical scroll position
     const scrollSpeed = 0.3;
@@ -8,40 +13,117 @@ window.addEventListener('scroll', function () {
 });
 
 document.addEventListener("DOMContentLoaded", function () {
+    const dropdownButton = document.getElementById("dropdown-button");
+    const dropdownContent = document.getElementById("dropdown-content");
+    
     updateNavCurrentPage();
+    handleResize();    
+
+    dropdownButton.addEventListener("click", () => {
+        dropdownContent.style.display =
+          dropdownContent.style.display === "block" ? "none" : "block";
+      });
+      
+      // Close dropdown if clicking outside
+      window.addEventListener("click", (event) => {
+        if (!dropdownButton.contains(event.target)) {
+          dropdownContent.style.display = "none";
+        }
+      });
 });
 
+window.addEventListener("resize", function() {
+    handleResize();
+    updateNavCurrentPage();
+});
+//#endregion
+
+//#region Functions
 function updateNavCurrentPage() {
-    let homeLink = document.getElementById("nav_home_link");
-    let projectsLink = document.getElementById("nav_projects_link");
-    let aboutLink = document.getElementById("nav_about_link");
-    let contactLink = document.getElementById("nav_contact_link");
+    let homeLinks = document.getElementsByClassName("nav_home_link");
+    let projectsLinks = document.getElementsByClassName("nav_projects_link");
+    let aboutLinks = document.getElementsByClassName("nav_about_link");
+    let contactLinks = document.getElementsByClassName("nav_contact_link");
 
     switch (window.location.pathname) {
         case "/index.html":
-            homeLink.style.fontWeight = "bold";
-            homeLink.style.color = "rgb(242, 97, 63)";
+            for (let element of homeLinks) {
+                element.style.fontWeight = "bold";
+                element.style.color = "rgb(242, 97, 63)";
+            }            
             break;
         
         case "/projects.html":
-            projectsLink.style.fontWeight = "bold";
-            projectsLink.style.color = "rgb(242, 97, 63)";
+            for (let element of projectsLinks) {
+                element.style.fontWeight = "bold";
+                element.style.color = "rgb(242, 97, 63)";
+            }
             break;
 
         case "/about.html":
-            aboutLink.style.fontWeight = "bold";
-            aboutLink.style.color = "rgb(242, 97, 63)";
+            for (let element of aboutLinks) {
+                element.style.fontWeight = "bold";
+                element.style.color = "rgb(242, 97, 63)";
+            }
             break;
 
         case "/contact.html":
-            contactLink.style.fontWeight = "bold";
-            contactLink.style.color = "rgb(242, 97, 63)";
+            for (let element of contactLinks) {
+                element.style.fontWeight = "bold";
+                element.style.color = "rgb(242, 97, 63)";
+            }
             break;   
         
         default:
-            homeLink.style.fontWeight = "bold";
-            homeLink.style.color = "rgb(242, 97, 63)";
+            for (let element of homeLinks) {
+                element.style.fontWeight = "bold";
+                element.style.color = "rgb(242, 97, 63)";
+            }
             console.error("updateNavCurrentPage(scripts.js): no html found; using default");
             break;
     }
 }
+
+function handleResize() {
+    const elementsToHideOn800 = document.getElementsByClassName("hide-on-800");
+    const elementsToHideOn530 = document.getElementsByClassName("hide-on-530");
+    const elementsToShowOn530 = document.getElementsByClassName("show-on-530");
+    const headerElement = document.getElementById("head");
+
+    if (window.innerWidth <= 530) {
+        for (let element of elementsToHideOn800) {
+            element.style.display = "none";
+        }
+        for (let element of elementsToHideOn530) {
+            element.style.display = "none";
+        }
+        for (let element of elementsToShowOn530) {
+            element.style.display = "block";
+        }
+        headerElement.className = "sticky grid grid-cols-1 items-center";
+    } else if (window.innerWidth < 800) {
+        for (let element of elementsToHideOn800) {
+            element.style.display = "none";
+        }
+        for (let element of elementsToHideOn530) {
+            element.style.display = "block";
+        }
+        for (let element of elementsToShowOn530) {
+            element.style.display = "none";
+        }
+        headerElement.className = "sticky grid grid-cols-1 items-center";
+    } else {
+        for (let element of elementsToHideOn800) {
+            element.style.display = "block";
+        }
+        for (let element of elementsToHideOn530) {
+            element.style.display = "block";
+        }
+        for (let element of elementsToShowOn530) {
+            element.style.display = "none";
+        }
+        headerElement.className = "sticky grid grid-cols-3 items-center";
+    }
+}
+//#endregion
+
