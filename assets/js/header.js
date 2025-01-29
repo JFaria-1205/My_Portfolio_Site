3e6f6d01b91e5b0b8a71517f81c5b2f18fb8cc4b
Jason Faria <jasonfaria01@gmail.com>
2025-01-29 15:07:50 -0500

    New icons, more homepage revamp
    revamped homepage to better suit window resizing with better code for more consistent and cleaner look; revamped a lot of the homepage code; changed some css for better look; NEW icon - custom made - imported and implemented.

--- assets/js/header.js
+++ assets/js/header.js
@@ -7,26 +7,26 @@ if (typeof siteHeader ==="undefined"){
             
             <nav class="hide-on-small font-thin text-xl z-10">
                 <ul class="flex justify-center space-x-10">
-                    <li><a href="index.html" class="nav_home_link link-hover">Home</a></li>
-                    <li><a href="projects.html" class="nav_projects_link link-hover">Projects</a></li>
-                    <li><a href="resume.html" class="nav_resume_link link-hover">Resume</a></li>
-                    <li><a href="about.html" class="nav_about_link link-hover">About</a></li>
-                    <li><a href="contact.html" class="nav_contact_link link-hover">Contact</a></li>
+                    <li><a href="index.html" class="nav_home_link nav-link">Home</a></li>
+                    <li><a href="projects.html" class="nav_projects_link nav-link">Projects</a></li>
+                    <li><a href="resume.html" class="nav_resume_link nav-link">Resume</a></li>
+                    <li><a href="about.html" class="nav_about_link nav-link">About</a></li>
+                    <li><a href="contact.html" class="nav_contact_link nav-link">Contact</a></li>
                 </ul>
             </nav>
             
             <a id="nav-extra" class="hide-on-medium font-thin text-lg text-right z-0" href="index.html">Portfolio</a>
 
             <ul id="header-small-menu" class="show-on-small font-thin text-xl z-10 items-center">
-                <li id="dropdown-button" class="link-hover-menu">
+                <li id="dropdown-button" class="nav-link-mini-menu">
                     <img id="small-nav-menu-pic" src="assets/images/nav-menu-button.png">
                 </li>
                 <ul id="dropdown-content" style="display: none;">
-                    <li><a href="index.html" class="nav_home_link link-hover-menu">Home</a></li>
-                    <li><a href="projects.html" class="nav_projects_link link-hover-menu">Projects</a></li>
-                    <li><a href="resume.html" class="nav_resume_link link-hover">Resume</a></li>
-                    <li><a href="about.html" class="nav_about_link link-hover-menu">About</a></li>
-                    <li><a href="contact.html" class="nav_contact_link link-hover-menu">Contact</a></li>
+                    <li><a href="index.html" class="nav_home_link nav-link-mini-menu">Home</a></li>
+                    <li><a href="projects.html" class="nav_projects_link nav-link-mini-menu">Projects</a></li>
+                    <li><a href="resume.html" class="nav_resume_link nav-link-mini-menu">Resume</a></li>
+                    <li><a href="about.html" class="nav_about_link nav-link-mini-menu">About</a></li>
+                    <li><a href="contact.html" class="nav_contact_link nav-link-mini-menu">Contact</a></li>
             </ul>    
         </header>
     `;
