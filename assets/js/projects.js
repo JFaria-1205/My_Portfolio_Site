c934bf5b0e01273a8f305cd73a0091f71fe1c324
Jason Faria <jasonfaria01@gmail.com>
2025-03-10 13:09:03 -0400

    project page breakpoints
    added breakpoints for project page, basically just mobile resizing

--- assets/js/projects.js
+++ assets/js/projects.js
@@ -20,36 +20,49 @@ async function onHoverEnter(element) {
     element.classList.remove("in-view-right");
     element.classList.remove("in-view-left");
     
-    element.classList.add("grow-in-progress");
-    await delay(350); //wait 0.35 seconds (1000=1s)
+    if (window.innerWidth > 768) {
+        // do this if px width is higher than 768
+        element.classList.add("grow-in-progress");
+        await delay(350); //wait 0.35 seconds (1000=1s)
 
-    if (element.classList.contains("grow-in-progress")) {
-        element.classList.add("enlarged");
+        if (element.classList.contains("grow-in-progress")) {
+            element.classList.add("enlarged");
 
-        const currentWidth = element.offsetWidth + "px";
-        element.style.width = currentWidth;
-        element.style.transition = "width 1s ease-out"
-        requestAnimationFrame(() => {
-            element.style.width = "60%";
-        });
+            const currentWidth = element.offsetWidth + "px";
+            element.style.width = currentWidth;
+            element.style.transition = "width 1s ease-out"
+            requestAnimationFrame(() => {
+                element.style.width = "60%";
+            });
     }    
+    }
+
+    
 }
 
 function onHoverExit(element) {
-    element.classList.remove("grow-in-progress")
 
-    if (element.classList.contains("enlarged")) {
-        element.classList.remove("enlarged");
+    
 
-        const currentWidth = element.offsetWidth + "px";
+    if (window.innerWidth > 768) {
+        // do this if px width is higher than 768
+        element.classList.remove("grow-in-progress");
 
-        element.style.width = currentWidth;
-        element.style.transition = "width 0.345s ease-out";
-
-        requestAnimationFrame(() => {
-            element.style.width = "44%";
-        });
+        if (element.classList.contains("enlarged")) {
+            element.classList.remove("enlarged");
+    
+            const currentWidth = element.offsetWidth + "px";
+    
+            element.style.width = currentWidth;
+            element.style.transition = "width 0.345s ease-out";
+    
+            requestAnimationFrame(() => {
+                element.style.width = "44%";
+            });
+        }
     }
+
+    
     
 }
 
